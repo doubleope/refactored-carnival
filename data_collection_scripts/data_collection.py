5f929d104b0eecee3915b40b298b9adc1dba9fa5
Francisco Baca <frankpbac246@gmail.com>
2020-02-17 17:00:25 -0600

    Change data

--- data_collection_scripts/data_collection.py
+++ data_collection_scripts/data_collection.py
@@ -6,6 +6,6 @@ companies = ["AAPL", "AMZN", "UBER", "NOK", "F", "GE", "FB", "NFLX", "GOOG",
             "PINS", "AMD", "MSFT", "IBM", "INTC", "BAC", "SNAP", "TWTR", "T"]
 
 for i in companies:
-    df = dr.get_data_yahoo(i, start = '2010-02-08', end = '2020-02-08')
+    df = dr.get_data_yahoo(i, start = '1950-02-08', end = '2020-02-08')
     df.to_csv("../data/"+ i +"_data.csv")
 
