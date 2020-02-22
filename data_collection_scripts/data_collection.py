d098a817e0b9977a010f38bb109198dd8b0ad8eb
Francisco Baca <frankpbac246@gmail.com>
2020-02-22 00:39:44 -0600

    Changes to data and data collection script

--- data_collection_scripts/data_collection.py
+++ data_collection_scripts/data_collection.py
@@ -2,10 +2,18 @@ import pandas as pd
 import pandas_datareader as dr
 import matplotlib
 
-companies = ["AAPL", "AMZN", "UBER", "NOK", "F", "GE", "FB", "NFLX", "GOOG",
-            "PINS", "AMD", "MSFT", "IBM", "INTC", "BAC", "SNAP", "TWTR", "T"]
 
-for i in companies:
-    df = dr.get_data_yahoo(i, start = '1950-02-08', end = '2020-02-08')
-    df.to_csv("../data/"+ i +"_data.csv")
+#this is for when I tried to use alpha vantage, yahoo is better in the end
+#available hourly data is insufficient, using daily instead
+#do not delete below comment in case we need it again
+# ts = TimeSeries(key=AV_key, output_format='pandas')
 
+
+FAANG = ['FB', 'AAPL', 'AMZN', 'NFLX', 'GOOG']
+# companies = ["AAPL", "AMZN", "UBER", "NOK", "F", "GE", "FB", "NFLX", "GOOG",
+#             "PINS", "AMD", "MSFT", "IBM", "INTC", "BAC", "SNAP", "TWTR", "T"]
+
+for company_name in FAANG:
+    #start date might say 1950, only goes as far as data is available
+    df = dr.get_data_yahoo(company_name, start = '1950-02-08', end = '2020-02-08')
+    df.to_csv("../data/"+ company_name +"_data.csv")
