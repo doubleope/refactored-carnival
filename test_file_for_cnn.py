bc409a1df5888488c7f8ec7ca6140f967dd0f566
Opeyemi Openiyi <doubleope@gmail.com>
2020-02-28 11:08:29 -0600

    prepare dataset and plot dataset with regards to training, validation, and test sets

---
+++ test_file_for_cnn.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from pandas import DatetimeIndex
+
+if __name__ == '__main__':
+    data_dir = './data'
+    datasource = "amzn"
+    data = pd.read_csv(os.path.join(data_dir, datasource + '.csv'), header=0, parse_dates={"timestamp": [0]})
+    dt_idx = DatetimeIndex(data.timestamp)
+
+    data = data.drop('timestamp', axis=1)
+
+    data = pd.DataFrame(data['High'])
+
+    data.index = dt_idx
+    print(data.tail())
+
+    valid_start_dt = '2013-12-06'
+    test_start_dt = '2017-01-12'
+
+    data[data.index < valid_start_dt][['High']].rename(columns={'High': 'train'}) \
+        .join(data[(data.index >= valid_start_dt) & (data.index < test_start_dt)][['High']] \
+              .rename(columns={'High': 'validation'}), how='outer') \
+        .join(data[test_start_dt:][['High']].rename(columns={'High': 'test'}), how='outer') \
+        .plot(y=['train', 'validation', 'test'], figsize=(15, 8), fontsize=12)
+    plt.xlabel('timestamp', fontsize=12)
+    plt.ylabel('High', fontsize=12)
+    plt.show()
